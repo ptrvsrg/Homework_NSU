ce45e4b6314be6aaa13d14c428795b27d66a4bf2
ptrvsrg <s.petrov1@g.nsu.ru>
2021-10-14 19:56:43 +0700

    Add cycle and recursion versions programm

---
+++ Задачи с одним списком/Циклы/Сумма элементов списка/main.c
@@ -0,0 +1,82 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TList
+{
+    unsigned int Value;
+    struct TList* Next;
+};
+
+typedef struct TList* TList;
+
+TList CreateList()
+{
+    return NULL;
+}
+
+void Push(unsigned int value, TList* list)
+{
+    TList new = malloc(sizeof(*new));
+    assert(new != NULL);
+    new->Value = value;
+    new->Next = *list;
+    (*list) = new;
+}
+
+void CreateListOfDigits(unsigned int number, TList* list)
+{
+    do
+    {
+        unsigned int digit = number % 10;
+        number /= 10;
+        Push(digit, list);
+    } while (number != 0);
+}
+
+int SumList(TList list)
+{
+    int sum = 0;
+
+    while(list)
+    {
+        sum += list->Value;
+        list = list->Next;
+    }
+
+    return sum;
+}
+
+void PrintList(TList list)
+{
+    while(list)
+    {
+        printf("%d ", list->Value);
+        list = list->Next;
+    }
+}
+
+void FreeList(TList* list) 
+{
+    TList buffer = NULL;
+
+    while ((*list)->Next) 
+    {
+        buffer = (*list);
+        (*list) = (*list)->Next;
+        free(buffer);
+    }
+
+    free(*list);
+}
+
+int main(void)
+{
+    TList list = CreateList();
+    CreateListOfDigits(461148, &list);
+    PrintList(list);
+    printf("\n%d\n", SumList(list));
+    FreeList(&list);
+    return 0;
+}
