5ea6912ebb84fff6496764b5fa947b96892ec006
ptrvsrg <s.petrov1@g.nsu.ru>
2022-02-27 20:20:16 +0700

    Add new homework

--- Разные задачи/Является ли число совершенным/main.c
+++ Разные задачи/Является ли число совершенным/main.c
@@ -1,33 +1,86 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int DividerCount(int number)
+typedef struct
 {
-    int power2 = 0;
+    int Count;
+    int Max;
+    int* Array;
+} TVector;
 
-    while (number & 1 == 0) 
-    {
-        number >>= 1;
-        ++power2;
-    }
+TVector CreateVector(int max)
+{
+    TVector vector = { 0, max, NULL };
 
-    return 2 * (power2 + 1);    
+    vector.Array = calloc(max, sizeof(*vector.Array));
+    assert(vector.Array != NULL);
+
+    return vector;
 }
 
-void isPerfect(int divider, int* sum, int num) 
+void PushVector(int value, TVector* vector)
 {
-    if (divider == 0)
+    assert(vector->Count != vector->Max);
+    vector->Array[vector->Count] = value;
+    ++vector->Count;
+}
+
+void PrintVector(TVector vector)
+{
+    for (int i = 0; i < vector.Count; ++i)
     {
-        /* code */
+        printf("%d ", vector.Array[i]);
+    }
+}
+
+void DestroyVector(TVector* vector)
+{
+    free(vector->Array);
+    vector->Count = 0;
+    vector->Max = 0;
+}
+
+bool IsPerfect(int num) 
+{
+    int sum = 0;
+    for (int div = 1; div <= num / 2; ++div) 
+    {
+        if (num % div == 0) 
+        {
+            sum += div;
+        }
+
+        if (sum > num)
+        {
+            return false;
+        }
+    }
+
+    return sum == num;
+}
+
+TVector PerfectNumber(int n)
+{
+    TVector vector = CreateVector(n);
+
+    int index = 0;
+    for (int i = 0; index != n && i < 16; ++i)
+    {
+        int number = (1 << i) * ((1 << (i + 1)) - 1);
+        if (IsPerfect(number))
+        {
+            PushVector(number, &vector);
+            ++index;
+        }
     }
     
-    if (num % divider == 0)
-    {
-        *sum += divider;
-        --divider;
-    }
+    return vector;
 }
 
 int main(void)
 {
-
+    PrintVector(PerfectNumber(5));
+    return EXIT_SUCCESS;
 }
