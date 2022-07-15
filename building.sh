5cc550d88f65245aa66e68a9915fceec441e8898
s.petrov1 <s.petrov1@g.nsu.ru>
2022-07-15 11:29:24 +0800

    Add exit status

--- building.sh
+++ building.sh
@@ -6,6 +6,7 @@ GREEN='\033[32m'
 RED='\033[31m'
 HOME_DIR=$(pwd)
 IFS=$'\n'
+EXIT_STATUS=0
 
 for path in $(find . -name "CMakeLists.txt" | sed -e "s/CMakeLists.txt//")
 do
@@ -18,9 +19,12 @@ do
 	if [ -s build/CMakeLog.txt ]
 	then
 		cat build/CMakeLog.txt
+        EXIT_STATUS=1
 		echo -e "${RED}${BOLD}Build failed${NORMAL}\n"
 	else
 		echo -e "${GREEN}${BOLD}Build completed${NORMAL}\n"
 	fi
 	cd ${HOME_DIR}
 done
+
+exit $EXIT_STATUS
